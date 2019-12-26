02fb5b512a0501d19cb43e0fbf94539941317883
Wojciech Walewski <w.walewski112@gmail.com>
2019-12-26 16:43:38 +0100

    Adds CORS to timerServer

--- ERAdmin/devices.js
+++ ERAdmin/devices.js
@@ -7,11 +7,11 @@ $(function(){
   function loadLatestResults(){
 
     $.ajax({
-      url : 'http://localhost:8080/devices/list',
+      url : 'http://192.168.0.145:8080/timer/status',
       cache : false,
-      dataType : "text",
+      dataType : "json",
       success : function(data){
-        $('#devices_foundcount').html(data);
+        $('#dev1-primary').html(data);
       },
       error: function (error) {
         console.log("error:" + error);
