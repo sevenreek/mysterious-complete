c3c83d48e22f55909a810f83f9f3557af2a76846
Wojciech Walewski <w.walewski112@gmail.com>
2020-01-11 19:33:09 +0100

    Adds python-vlc to bash

--- rpi_configure.bash
+++ rpi_configure.bash
@@ -3,7 +3,9 @@ sudo apt -y update
 sudo apt -y install xrdp
 sudo systemctl enable ssh
 sudo systemctl start ssh
+sudo pip3 install --upgrade pip
 sudo pip3 install bottle
 sudo pip3 install adafruit-circuitpython-ht16k33
 sudo pip3 install adafruit-blinka
+sudo pip3 install python-vlc
 
