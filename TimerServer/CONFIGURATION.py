09c8dbf5802349bbdf75112d115f21cd04503255
Wojciech Walewski <w.walewski112@gmail.com>
2020-02-19 23:29:20 +0100

    Changes CONFIGURATION order and adds readme

--- TimerServer/CONFIGURATION.py
+++ TimerServer/CONFIGURATION.py
@@ -1,12 +1,12 @@
 # Feel free to edit these
 CFG_ROOM_NAME = "Test Room #1"
 CFG_DEFAULT_TIME = 3600 # in seconds
+CFG_ROOM_UNIQUE_ID = 0xDE00 # device ID has to have the format of 0xDE%%
+# Probably don't touch these
 CFG_LOGS_DIR = 'logs'
 CFG_LOGS_DATE_FORMAT = '%Y-%m-%d'
 CFG_LOGS_TIME_FORMAT = '[%H:%M:%S] '
 CFG_LOGS_DAYS_ARCHIVE_SIZE = 7
-# Probably don't touch these
-CFG_ROOM_UNIQUE_ID = 0xDE00 # device ID has to have the format of 0xDE%%
 CFG_UDP_DETECT_BROADCAST_PORT = 4000
 CFG_HTTP_SERVER_PORT = 8080
 CFG_HTTP_SEVER_HOST = '0.0.0.0'
